8756df06f11331cc0775d43587db86c853e12a73
BenjaminBoho <tongbenboho@gmail.com>
2023-08-14 16:43:40 +0900

    Initial Commit

---
+++ Mieruka/MierukaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MierukaApp.swift
+//  Mieruka
+//
+//  Created by れい on 2023/08/14.
+//
+
+import SwiftUI
+
+@main
+struct MierukaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
